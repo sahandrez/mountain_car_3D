c161830626587a93fbd5a332507b9eebfcfc4058
Sahand <sahand.rezaei.sh@gmail.com>
2022-04-18 20:33:57 -0400

    Change length to 1000 steps

--- mountain_car_3d/__init__.py
+++ mountain_car_3d/__init__.py
@@ -6,6 +6,6 @@ from gym.envs.registration import register
 register(
     id='MountainCarContinuous3D-v0',
     entry_point='mountain_car_3d.envs:Continuous_MountainCar3DEnv',
-    max_episode_steps=999,
+    max_episode_steps=1000,
     reward_threshold=90.0,
 )
